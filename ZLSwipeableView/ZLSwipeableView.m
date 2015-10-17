9daaaa3619e872ed005fdc5d365a4080b66c767d
buffpojken <daniel@sykewarrior.com>
2015-10-17 16:59:44 +0200

    debugging memory leak

--- ZLSwipeableView/ZLSwipeableView.m
+++ ZLSwipeableView/ZLSwipeableView.m
@@ -475,9 +475,9 @@ ZLDirectionVectorToSwipeableViewDirection(CGVector directionVector) {
         [self.animator addBehavior:be];
     }
 
-    [self.reuseCoverContainerView addSubview:self.anchorView];
-    [self.reuseCoverContainerView addSubview:swipeableView];
-    [self.reuseCoverContainerView sendSubviewToBack:swipeableView];
+    // [self.reuseCoverContainerView addSubview:self.anchorView];
+    // [self.reuseCoverContainerView addSubview:swipeableView];
+    // [self.reuseCoverContainerView sendSubviewToBack:swipeableView];
 
     self.anchorView = nil;
 
